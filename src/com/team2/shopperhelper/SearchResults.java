84d715fba50ee9bfec804a2bc93f840cdff4b630
danac78 <danacraig78@gmail.com>
2012-07-17 18:45:38 -0700

    Umm...Forgot one

---
+++ src/com/team2/shopperhelper/SearchResults.java
@@ -0,0 +1,45 @@
+package com.team2.shopperhelper;
+
+public class SearchResults {
+
+	private String store = "";
+	private String address = "";
+	private String secondAddress="";
+	/**
+	 * @return the store
+	 */
+	public String getStore() {
+		return store;
+	}
+	/**
+	 * @param store the store to set
+	 */
+	public void setStore(String store) {
+		this.store = store;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the secondAddress
+	 */
+	public String getSecondAddress() {
+		return secondAddress;
+	}
+	/**
+	 * @param secondAddress the secondAddress to set
+	 */
+	public void setSecondAddress(String secondAddress) {
+		this.secondAddress = secondAddress;
+	}
+	
+}
