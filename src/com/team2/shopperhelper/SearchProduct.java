c9fd14f57420bacf45e41c30dc9a52e80c0927bd
danac78 <danacraig78@gmail.com>
2012-07-21 22:02:48 -0700

    Fixed Issue 6 as well as adjusted some comments.

--- src/com/team2/shopperhelper/SearchProduct.java
+++ src/com/team2/shopperhelper/SearchProduct.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 /**
  * @author Dana Haywood
  * @date 7/12/2012
- * @version 0.1.2
+ * @version 0.1.5
  * @IT482
  * @Karl Lloyd
  * @Source Cite http://developer.android.com/guide/components/index.html
