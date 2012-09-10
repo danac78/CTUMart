69ead4f08862fe7004acbae7b282e15619986177
Dana Haywood <danacraig78@gmail.com>
2012-09-10 06:17:25 -0700

    Update src/com/team2/shopperhelper/Launch.java

--- src/com/team2/shopperhelper/Launch.java
+++ src/com/team2/shopperhelper/Launch.java
@@ -14,8 +14,8 @@ import com.team2.shopperhelper.library.DialogBox;
  * Activity to check Internet and Compatibility issues prior to proceeding.
  * 
  * @author Dana Haywood
- * @version 0.9.5
- * @since 9/1/2012<br>
+ * @version 1.0.0
+ * @since 9/10/2012<br>
  * 
  *        Instructor: Karl Lloyd<br>
  *        Class: IT482<br>
